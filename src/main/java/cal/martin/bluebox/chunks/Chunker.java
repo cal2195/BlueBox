23525ac2960bcbe920160bf8705a9ea4c4d99abf
cal2195 <cmartin7@tcd.ie>
2016-07-21 19:49:34 +0100

    Adding class structure.

---
+++ src/main/java/cal/martin/bluebox/chunks/Chunker.java
@@ -0,0 +1,18 @@
+package cal.martin.bluebox.chunks;
+
+/**
+ *
+ * @author cal
+ */
+public class Chunker
+{
+    public byte[] getChunk(int chunkID)
+    {
+        
+    }
+    
+    public boolean saveChunk(int chunkID, byte[] bytes)
+    {
+        
+    }
+}
---
+++ src/main/java/cal/martin/bluebox/fuse/FUSE.java
@@ -0,0 +1,12 @@
+package cal.martin.bluebox.fuse;
+
+import ru.serce.jnrfuse.FuseStubFS;
+
+/**
+ *
+ * @author cal
+ */
+public class FUSE extends FuseStubFS
+{
+    
+}
